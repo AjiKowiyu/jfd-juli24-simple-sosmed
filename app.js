1ee62d8e64cb6179c2c0813a8846da9c70cd1ea3
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-08-19 19:38:01 +0700

    setting aplikasi & route awal

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Simple Sosmed')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+
+
+app.use( express.urlencoded({extended:false}) )
+app.set('view engine', 'ejs')
+app.set('views', './view')
+
+
+app.get('/', c_beranda.index)
+
+
+app.listen(port, ()=>{
+    console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+})
